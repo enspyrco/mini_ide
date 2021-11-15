75f0c3c28ed67318f9f47f95ad85123e8ce5987f
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-16 10:17:32 +1100

    Use IFrameElement & HtmlElementView to show editor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
+final IFrameElement _iframeElement = IFrameElement()
+  ..style.width = '100%'
+  ..style.height = '100%'
+  ..width = '200.0'
+  ..height = '200.0'
+  ..src = 'inner.html'
+  ..style.border = 'none';
+Widget _iframeWidget = HtmlElementView(
+  key: UniqueKey(),
+  viewType: 'iframeElement',
+);
+
 void main() {
+  // ignore: undefined_prefixed_name
+  ui.platformViewRegistry.registerViewFactory(
+    'iframeElement',
+    (int viewId) => _iframeElement,
+  );
   runApp(const MyApp());
 }
 
@@ -10,11 +31,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Text('Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SizedBox(
+          height: 200,
+          width: 200,
+          child: _iframeWidget,
+        ));
   }
 }
